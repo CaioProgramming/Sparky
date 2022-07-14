f400f9acc50e396c8e4bf6cf14a1bbd8aa8dee2b
CaioProgramming <caioalvesferreiracontato@gmail.com>
2022-07-14 16:32:41 -0300

    moved paths to constants.kt

--- core/src/main/java/com/silent/core/users/UsersService.kt
+++ core/src/main/java/com/silent/core/users/UsersService.kt
@@ -2,10 +2,11 @@ package com.silent.core.users
 
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.QueryDocumentSnapshot
+import com.silent.core.utils.USERS_PATH
 import com.silent.ilustriscore.core.model.BaseService
 
 class UsersService : BaseService() {
-    override val dataPath = "Users"
+    override val dataPath = USERS_PATH
     override var requireAuth = true
 
     override fun deserializeDataSnapshot(dataSnapshot: DocumentSnapshot): User {
