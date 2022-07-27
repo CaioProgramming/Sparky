44cfdeb0408fb9faf0c447c690b13955f989b82a
CaioProgramming <caioalvesferreiracontato@gmail.com>
2022-07-27 09:19:20 -0300

    checking sdk version to set up right flag

--- core/src/main/java/com/silent/core/notifications/SparkyMessagingService.kt
+++ core/src/main/java/com/silent/core/notifications/SparkyMessagingService.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.media.RingtoneManager
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -17,6 +18,7 @@ import com.silent.core.utils.TOKEN_PREFERENCES
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import kotlin.random.Random
 
 
 class SparkyMessagingService : FirebaseMessagingService() {
@@ -30,14 +32,18 @@ class SparkyMessagingService : FirebaseMessagingService() {
         message.notification?.let { notification ->
             val icon = ImageUtils.getNotificationIcon(notification.icon)
             var defaultColor = baseContext.getColor(R.color.material_yellow700)
+            val podcastId = message.data["podcastId"]
             notification.color?.let {
                 defaultColor = Color.parseColor(it)
             }
             val homeIntent = Intent(this, Class.forName(HOME_ACT)).apply {
                 addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                putExtra("podcastId", message.data["podcastId"])
+                putExtra("podcastId", podcastId)
             }
-            val pendingIntent = PendingIntent.getActivity(this, 0, homeIntent, PendingIntent.FLAG_CANCEL_CURRENT)
+            val notificationId = Random.nextInt(podcastId?.length ?: 0)
+            val flag =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) PendingIntent.FLAG_IMMUTABLE else PendingIntent.FLAG_CANCEL_CURRENT
+            val pendingIntent = PendingIntent.getActivity(this, 0, homeIntent, flag)
             val channelId = getString(R.string.channel_id)
             val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
             val notificationBuilder = NotificationCompat.Builder(this, channelId)
@@ -51,7 +57,7 @@ class SparkyMessagingService : FirebaseMessagingService() {
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent)
             val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.notify(1, notificationBuilder.build())
+            notificationManager.notify(notificationId, notificationBuilder.build())
         }
 
     }
